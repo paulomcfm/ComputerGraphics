826e2cf0002f37aefbb6d5e2b8bbd55f20e8361a
paulomcfm <paulomcfm@gmail.com>
2025-02-16 09:52:40 -0300

    Primeiro commit

---
+++ ProcessamentoImagens/frmPrincipal.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace ProcessamentoImagens
+{
+    public partial class frmPrincipal : Form
+    {
+        private Image image;
+        private Bitmap imageBitmap;
+
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrirImagem_Click(object sender, EventArgs e)
+        {
+            openFileDialog.FileName = "";
+            openFileDialog.Filter = "Arquivos de Imagem (*.jpg;*.gif;*.bmp;*.png)|*.jpg;*.gif;*.bmp;*.png";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                image = Image.FromFile(openFileDialog.FileName);
+                pictBoxImg1.Image = image;
+                pictBoxImg1.SizeMode = PictureBoxSizeMode.Normal;
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            pictBoxImg1.Image = null;
+            pictBoxImg2.Image = null;
+        }
+
+        private void btnLuminanciaSemDMA_Click(object sender, EventArgs e)
+        {
+            Bitmap imgDest = new Bitmap(image);
+            imageBitmap = (Bitmap)image;
+            Filtros.convert_to_gray(imageBitmap, imgDest);
+            pictBoxImg2.Image = imgDest;
+        }
+
+        private void btnLuminanciaComDMA_Click(object sender, EventArgs e)
+        {
+            Bitmap imgDest = new Bitmap(image);
+            imageBitmap = (Bitmap)image;
+            Filtros.convert_to_grayDMA(imageBitmap, imgDest);
+            pictBoxImg2.Image = imgDest;
+        }
+
+        private void btnNegativoSemDMA_Click(object sender, EventArgs e)
+        {
+            Bitmap imgDest = new Bitmap(image);
+            imageBitmap = (Bitmap)image;
+            Filtros.negativo(imageBitmap, imgDest);
+            pictBoxImg2.Image = imgDest;
+        }
+
+        private void btnNegativoComDMA_Click(object sender, EventArgs e)
+        {
+            Bitmap imgDest = new Bitmap(image);
+            imageBitmap = (Bitmap)image;
+            Filtros.negativoDMA(imageBitmap, imgDest);
+            pictBoxImg2.Image = imgDest;
+        }
+    }
+}
---
+++ ProcessamentoImagens/frmPrincipal.designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ProcessamentoImagens
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictBoxImg1 = new System.Windows.Forms.PictureBox();
+            this.pictBoxImg2 = new System.Windows.Forms.PictureBox();
+            this.btnAbrirImagem = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnLuminanciaSemDMA = new System.Windows.Forms.Button();
+            this.btnLuminanciaComDMA = new System.Windows.Forms.Button();
+            this.btnNegativoComDMA = new System.Windows.Forms.Button();
+            this.btnNegativoSemDMA = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxImg1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxImg2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictBoxImg1
+            // 
+            this.pictBoxImg1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictBoxImg1.Location = new System.Drawing.Point(5, 6);
+            this.pictBoxImg1.Name = "pictBoxImg1";
+            this.pictBoxImg1.Size = new System.Drawing.Size(600, 500);
+            this.pictBoxImg1.TabIndex = 102;
+            this.pictBoxImg1.TabStop = false;
+            // 
+            // pictBoxImg2
+            // 
+            this.pictBoxImg2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictBoxImg2.Location = new System.Drawing.Point(611, 6);
+            this.pictBoxImg2.Name = "pictBoxImg2";
+            this.pictBoxImg2.Size = new System.Drawing.Size(600, 500);
+            this.pictBoxImg2.TabIndex = 105;
+            this.pictBoxImg2.TabStop = false;
+            // 
+            // btnAbrirImagem
+            // 
+            this.btnAbrirImagem.Location = new System.Drawing.Point(5, 512);
+            this.btnAbrirImagem.Name = "btnAbrirImagem";
+            this.btnAbrirImagem.Size = new System.Drawing.Size(101, 23);
+            this.btnAbrirImagem.TabIndex = 106;
+            this.btnAbrirImagem.Text = "Abrir Imagem";
+            this.btnAbrirImagem.UseVisualStyleBackColor = true;
+            this.btnAbrirImagem.Click += new System.EventHandler(this.btnAbrirImagem_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(112, 512);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(101, 23);
+            this.btnLimpar.TabIndex = 107;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnLuminanciaSemDMA
+            // 
+            this.btnLuminanciaSemDMA.Location = new System.Drawing.Point(219, 512);
+            this.btnLuminanciaSemDMA.Name = "btnLuminanciaSemDMA";
+            this.btnLuminanciaSemDMA.Size = new System.Drawing.Size(208, 23);
+            this.btnLuminanciaSemDMA.TabIndex = 108;
+            this.btnLuminanciaSemDMA.Text = "Luminância sem DMA";
+            this.btnLuminanciaSemDMA.UseVisualStyleBackColor = true;
+            this.btnLuminanciaSemDMA.Click += new System.EventHandler(this.btnLuminanciaSemDMA_Click);
+            // 
+            // btnLuminanciaComDMA
+            // 
+            this.btnLuminanciaComDMA.Location = new System.Drawing.Point(219, 541);
+            this.btnLuminanciaComDMA.Name = "btnLuminanciaComDMA";
+            this.btnLuminanciaComDMA.Size = new System.Drawing.Size(208, 23);
+            this.btnLuminanciaComDMA.TabIndex = 109;
+            this.btnLuminanciaComDMA.Text = "Luminância com DMA";
+            this.btnLuminanciaComDMA.UseVisualStyleBackColor = true;
+            this.btnLuminanciaComDMA.Click += new System.EventHandler(this.btnLuminanciaComDMA_Click);
+            // 
+            // btnNegativoComDMA
+            // 
+            this.btnNegativoComDMA.Location = new System.Drawing.Point(433, 541);
+            this.btnNegativoComDMA.Name = "btnNegativoComDMA";
+            this.btnNegativoComDMA.Size = new System.Drawing.Size(208, 23);
+            this.btnNegativoComDMA.TabIndex = 111;
+            this.btnNegativoComDMA.Text = "Negativo com DMA";
+            this.btnNegativoComDMA.UseVisualStyleBackColor = true;
+            this.btnNegativoComDMA.Click += new System.EventHandler(this.btnNegativoComDMA_Click);
+            // 
+            // btnNegativoSemDMA
+            // 
+            this.btnNegativoSemDMA.Location = new System.Drawing.Point(433, 512);
+            this.btnNegativoSemDMA.Name = "btnNegativoSemDMA";
+            this.btnNegativoSemDMA.Size = new System.Drawing.Size(208, 23);
+            this.btnNegativoSemDMA.TabIndex = 110;
+            this.btnNegativoSemDMA.Text = "Negativo sem DMA";
+            this.btnNegativoSemDMA.UseVisualStyleBackColor = true;
+            this.btnNegativoSemDMA.Click += new System.EventHandler(this.btnNegativoSemDMA_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1220, 608);
+            this.Controls.Add(this.btnNegativoComDMA);
+            this.Controls.Add(this.btnNegativoSemDMA);
+            this.Controls.Add(this.btnLuminanciaComDMA);
+            this.Controls.Add(this.btnLuminanciaSemDMA);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnAbrirImagem);
+            this.Controls.Add(this.pictBoxImg2);
+            this.Controls.Add(this.pictBoxImg1);
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Formulário Principal";
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxImg1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxImg2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictBoxImg1;
+        private System.Windows.Forms.PictureBox pictBoxImg2;
+        private System.Windows.Forms.Button btnAbrirImagem;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnLuminanciaSemDMA;
+        private System.Windows.Forms.Button btnLuminanciaComDMA;
+        private System.Windows.Forms.Button btnNegativoComDMA;
+        private System.Windows.Forms.Button btnNegativoSemDMA;
+    }
+}
+
