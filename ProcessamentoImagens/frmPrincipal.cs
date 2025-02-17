53b87e075b35ed78360042ed1d6b8f8235400697
Daniel Elias Fonseca Rumin <43758059+MrBElga@users.noreply.github.com>
2025-02-17 16:40:52 -0300

    Update: separar Bandas com DMA

--- ProcessamentoImagens/frmPrincipal.cs
+++ ProcessamentoImagens/frmPrincipal.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Windows.Forms;
 using System.IO;
 using System.Drawing.Imaging;
-
+    
 namespace ProcessamentoImagens
 {
     public partial class frmPrincipal : Form
@@ -36,6 +36,7 @@ namespace ProcessamentoImagens
             }
         }
 
+        /*
         private void updatePictures(Bitmap imageBitmap)
         {
             if (imageBitmap != null)
@@ -57,6 +58,7 @@ namespace ProcessamentoImagens
                     Filtros.convert_to_grayDMA(imageBitmap, imgH, 'H');
                     Filtros.convert_to_grayDMA(imageBitmap, imgS, 'S');
                     Filtros.convert_to_grayDMA(imageBitmap, imgI, 'I');
+
 
                     // Criar a imagem HSI em tons de cinza
                     for (int y = 0; y < image.Height; y++)
@@ -94,22 +96,118 @@ namespace ProcessamentoImagens
                 }
             }
         }
+        */
 
-
-        private int normaliza(int valor)
+        private void updatePictures(Bitmap imageBitmap)
         {
-            if (valor > 255)
+          
+            if (imageBitmap != null)
             {
-                valor = 255; 
+                try
+                {
+                    Bitmap imgRed = new Bitmap(image.Width, image.Height);
+                    Bitmap imgGreen = new Bitmap(image.Width, image.Height);
+                    Bitmap imgBlue = new Bitmap(image.Width, image.Height);
+
+                    Bitmap imgH = new Bitmap(image.Width, image.Height);
+                    Bitmap imgS = new Bitmap(image.Width, image.Height);
+                    Bitmap imgI = new Bitmap(image.Width, image.Height);
+
+                    // Converter para tons de cinza
+                    Filtros.convert_to_grayDMA(imageBitmap, imgRed, 'R');
+                    Filtros.convert_to_grayDMA(imageBitmap, imgGreen, 'G');
+                    Filtros.convert_to_grayDMA(imageBitmap, imgBlue, 'B');
+                    Filtros.convert_to_grayDMA(imageBitmap, imgH, 'H');
+                    Filtros.convert_to_grayDMA(imageBitmap, imgS, 'S');
+                    Filtros.convert_to_grayDMA(imageBitmap, imgI, 'I');
+
+                    
+               
+                    pictBoxRed.Image = new Bitmap(imgRed, pictBoxRed.Size);
+                    pictBoxGreen.Image = new Bitmap(imgGreen, pictBoxGreen.Size);
+                    pictBoxBlue.Image = new Bitmap(imgBlue, pictBoxBlue.Size);
+                    pictBoxH.Image = new Bitmap(separarHsiComDma(imgH,'H'), pictBoxH.Size);
+                    pictBoxS.Image = new Bitmap(separarHsiComDma(imgS, 'S'), pictBoxS.Size);
+                    pictBoxI.Image = new Bitmap(separarHsiComDma(imgI, 'I'), pictBoxI.Size);
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ocorreu um erro ao processar a imagem: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
-            if (valor < 0)
+        }
+        private Bitmap separarHsiComDma(Bitmap imageBitmap, char tipo)
+        {
+            int width = imageBitmap.Width;
+            int height = imageBitmap.Height;
+            int pixelSize = 3;
+
+
+            Bitmap destinationBitmap = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+
+            // Lock da imagem de origem
+            BitmapData bitmapDataSrc = imageBitmap.LockBits(new Rectangle(0, 0, width, height),
+                ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+
+            // Lock da imagem de destino
+            BitmapData bitmapDataDst = destinationBitmap.LockBits(new Rectangle(0, 0, width, height),
+                ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+
+            int padding = bitmapDataSrc.Stride - (width * pixelSize);
+
+            unsafe
             {
-                valor = 0;
+                byte* src = (byte*)bitmapDataSrc.Scan0.ToPointer();
+                byte* dst = (byte*)bitmapDataDst.Scan0.ToPointer();
+
+                int r, g, b;
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        b = *(src++); 
+                        g = *(src++); 
+                        r = *(src++); 
+
+                        if (tipo == 'H')
+                        {
+                            int h = (int)hsi[x, y].Hue;
+                            h = Math.Min(255, Math.Max(0, h * 255 / 360));
+                            *(dst++) = (byte)h;
+                            *(dst++) = (byte)h;
+                            *(dst++) = (byte)h;
+                        }
+                        else if (tipo == 'S')
+                        {
+                            int s = (int)hsi[x, y].Saturation;
+                            s = Math.Min(100, Math.Max(0, s * 255 / 100));
+                            *(dst++) = (byte)s;
+                            *(dst++) = (byte)s;
+                            *(dst++) = (byte)s;
+                        }
+                        else if (tipo == 'I')
+                        {
+                            int i = (int)hsi[x, y].Intensity;
+                            i = Math.Min(255, Math.Max(0, i));
+                            *(dst++) = (byte)i;
+                            *(dst++) = (byte)i;
+                            *(dst++) = (byte)i;
+                        }
+                    }
+                    src += padding; 
+                    dst += padding; 
+                }
             }
 
-            return valor;
+            imageBitmap.UnlockBits(bitmapDataSrc);
+            destinationBitmap.UnlockBits(bitmapDataDst);
+
+            return destinationBitmap;
         }
- 
+
+
+
         private void btnLimpar_Click(object sender, EventArgs e)
         {
             pictBoxImg.Image = null;
